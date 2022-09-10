14f14b3a7ef4bee63c4ed60efa9df59cf9277ea4
techryter <kelvinamamize@gmail.com>
2022-09-09 22:47:51 -0700

    task 3 commit

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,15 +1,23 @@
 #include <stdio.h>
 
-/* printing the alphabet */
+/**
+*main - print alphabet in lower and uppercase
+*
+*
+* Return: (0) Success
+*/
 int main(void)
 {
 	char ch;
+
 	for (ch = 'a' ; ch <= 'z' ; ch++)
+	{
 		putchar(ch);
-
+	}
 	for (ch = 'A' ; ch <= 'Z' ; ch++)
+	{
 		putchar(ch);
-		putchar('\n');
-
+	}
+	putchar('\n');
 	return (0);
 }
