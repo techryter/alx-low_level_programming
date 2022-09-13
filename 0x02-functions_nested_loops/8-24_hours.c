dd645f6e13bbd4064809b10428a52cf18223a1a1
techryter <kelvinamamize@gmail.com>
2022-09-13 14:47:47 -0700

    Task 8 error commit

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,6 +1,7 @@
 #include "main.h"
 /**
- * jack_bauer - funtion that prints every minute of the day of Jack Bauer, starting from 00:00 to 23:59. min loop counts mins, while hour loop counts
+ * jack_bauer - funt that prints every min of the day of Jack Bauer, start
+ * ing from 00:00 to 23:59. min loop counts mins, while hour loop counts
  * hours and resets mins
  * Return:0
  */
