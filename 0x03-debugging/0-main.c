47ab6d0650004e2d3a2f9a9c5cc1f1d82bbb7f39
techryter <kelvinamamize@gmail.com>
2022-09-15 06:43:39 -0700

    Debugging task 0 commit

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - tests fnction that prints if integer is positive or negative
+ * Returns: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
