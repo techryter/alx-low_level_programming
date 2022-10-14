f90496c1413ce6a7d01a83a39b8543f016424caf
techryter <kelvinamamize@gmail.com>
2022-10-13 20:20:54 -0700

    task 2

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -9,7 +9,8 @@
  *
  * Return: void
  */
-void print_strings(const char *separator, const unsigned int n, ...){
+void print_strings(const char *separator, const unsigned int n, ...)
+{
 	unsigned int i;
 	char *str;
 	va_list valist;
